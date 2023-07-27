7fdf78a7a9ebb230822acd47e0ee860a921552e0
Victor Lavrentyev <v.lavrentv@gmail.com>
2023-07-27 17:00:25 +0200

    fix platform minimum supported platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import PackageDescription
 let package = Package(
     name: "Queenfisher",
 	platforms: [
-		.macOS(.v10_15)
+		.macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13)
 	],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
